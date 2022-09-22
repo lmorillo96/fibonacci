61514abdcfa1f9b75fc64e2924d1db285f6d2005
Leandro Morillo <leandro.m9601@gmail.com>
2022-09-21 22:19:24 -0500

    add array

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function fibonacci(num) {
+  fibArray = [];
+
   let i;
   let fib = [1, 0];
 
   for (i = 2; i <= num; i++) {
     fib[i] = fib[i - 2] + fib[i - 1];
-    console.log(fib[i]);
+    fibArray.push(fib[i]);
   }
 }
 
 fibonacci(7);
+console.log(fibArray);
